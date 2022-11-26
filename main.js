8301e91a247ad7f54a98d9a7e7267e002d16726c
Bimax0408 <104275456+Bimax0408@users.noreply.github.com>
2022-11-26 12:36:49 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function elon() {
+    window.location="elon.html";
+} 
+
+function regresar() {
+    window.location="index.html";
+}
+
+function jeff() {
+    window.location="jeff.html";
+} 
+
+function bernard() {
+    window.location="bernard.html";
+} 
